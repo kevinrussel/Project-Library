c60226cf6a8632698867fdff66d1998ba855262d
kzone159 <kzone159@gmail.com>
2024-09-13 12:56:34 -0400

    added the html file

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("hello there")
+/// what I think I wan't to do is the way thatI am going to approach this is by 
+// first creating a book array?
+
+
+class Book{
+    pages;
+    title;
+
+    constructor(pages, title){
+        this.pages = pages;
+        this.title = title;
+    }
+
+
+}
